644d2629dc3fd7ade7b1eb185da905427debaaf3
Patryk Janicki <patryk.janicki@beefee.co.uk>
2025-11-23 21:17:54 +0100

    test timer

--- reactproject/src/main.jsx
+++ reactproject/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const element = React.createElement('a', { href: 'https://google.pl' }, 'test');
+console.log(<h1>test</h1>);
+console.log(element);
+
+const currentDate = new Date();
+
+const hours = currentDate.getHours();
+const minutes = currentDate.getMinutes();
+const seconds = currentDate.getSeconds();
+console.log(hours, minutes);
+
+const timer = (
+  <div>
+    {hours}:{minutes}:{seconds}
+  </div>
+);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <StrictMode>{timer}</StrictMode>
+);
