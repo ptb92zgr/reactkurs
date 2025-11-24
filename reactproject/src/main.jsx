353120d06cb354353f1d210438db8d602d3d1dae
Patryk Janicki <patryk.janicki@beefee.co.uk>
2025-11-24 19:54:29 +0100

    JSX possibilities

--- reactproject/src/main.jsx
+++ reactproject/src/main.jsx
@@ -1,24 +1,9 @@
-import React from 'react';
+// import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-const element = React.createElement('a', { href: 'https://google.pl' }, 'test');
-console.log(<h1>test</h1>);
-console.log(element);
-
-const currentDate = new Date();
-
-const hours = currentDate.getHours();
-const minutes = currentDate.getMinutes();
-const seconds = currentDate.getSeconds();
-console.log(hours, minutes);
-
-const timer = (
-  <div>
-    {hours}:{minutes}:{seconds}
-  </div>
-);
+const element = <div></div>;
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>{timer}</StrictMode>
+  <StrictMode>{element}</StrictMode>
 );
