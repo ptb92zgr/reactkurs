303b1b59ef3044166ca8764d58aee1f25fd1d449
Patryk Janicki <patryk.janicki@beefee.co.uk>
2025-11-25 20:50:23 +0100

    conditional rendering

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { PersonInfo } from './components/PersonInfo';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const jan = {
+    name: 'Jan',
+    lastName: 'Kowalski',
+    mail: ' j.kowalski@gmail.com',
+    tel: '123456789',
+  };
+  const janina = {
+    name: 'Janina',
+    lastName: 'Kowalska',
+    mail: ' j.kowalska@gmail.com',
+    tel: '123456999',
+  };
+  const karol = {
+    name: 'Karol',
+    lastName: 'Kowalski',
+    mail: ' k.kowalski@gmail.com',
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <PersonInfo person={jan} />
+      <br />
+      <PersonInfo person={janina} />
+      <br />
+      <PersonInfo person={karol} />
     </>
   );
 }
--- reactproject/src/main.jsx
+++ reactproject/src/main.jsx
@@ -1,31 +1,5 @@
 // import React from 'react';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Link } from './components/Link';
-import { HeadingItalic } from './components/HeadingItalic';
-import { Logger } from './components/Logger';
+import App from './App.jsx';
 
-const element = (
-  <>
-    <Logger>
-      <HeadingItalic>Nasza strona</HeadingItalic>
-    </Logger>
-
-    <main>
-      <Logger>
-        <HeadingItalic>Nasza strona</HeadingItalic>
-      </Logger>
-      <Link text='Google' shouldOpenNewTab={true} />
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum
-        inventore facilis deleniti perferendis illum dolor velit neque
-        blanditiis optio iste.
-      </p>
-      <Link shouldOpenNewTab={false} />
-    </main>
-  </>
-);
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>{element}</StrictMode>
-);
+createRoot(document.getElementById('root')).render(<App />);
